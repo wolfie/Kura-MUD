41753293d45eea9a21319b6c4ec815c3d4359a15
kim <kimleppanen@client-184.office.itmill.com>
2011-02-28 08:24:10 +0200

    Fixed attack command

--- src/com/github/wolfie/kuramud/client/commands/combat/Attack.java
+++ src/com/github/wolfie/kuramud/client/commands/combat/Attack.java
@@ -7,24 +7,30 @@ import com.github.wolfie.kuramud.server.PlayerCharacter;
 
 public class Attack implements Command {
 
-  private static final long serialVersionUID = 4292250342764055479L;
+    private static final long serialVersionUID = 4292250342764055479L;
 
-  private final PlayerCharacter player;
+    private final PlayerCharacter player;
 
-  public Attack(final PlayerCharacter player) {
-    this.player = player;
-  }
+    public Attack(final PlayerCharacter player) {
+        this.player = player;
+    }
 
-  @Override
-  public Object execute(final Console console, final String[] argv)
-      throws Exception {
-    player.attack("");
-    return null;
-  }
+    @Override
+    public Object execute(final Console console, final String[] argv)
+            throws Exception {
+        String arguments = "";
+        if (argv != null && argv.length > 0) {
+            for (int i = 1; i < argv.length; i++) {
+                arguments += argv[i];
+            }
+        }
+        player.attack(arguments);
+        return null;
+    }
 
-  @Override
-  public String getUsage(final Console console, final String[] argv) {
-    return "Attack players or mobs in the same room";
-  }
+    @Override
+    public String getUsage(final Console console, final String[] argv) {
+        return "Attack players or mobs in the same room";
+    }
 
 }
