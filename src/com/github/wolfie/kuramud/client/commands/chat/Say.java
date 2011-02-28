338bde36dcef5ca4c97609afd82ab36eeac93bd4
kim <kimleppanen@client-184.office.itmill.com>
2011-02-28 08:18:52 +0200

    Fixed "say"-bug

--- src/com/github/wolfie/kuramud/client/commands/chat/Say.java
+++ src/com/github/wolfie/kuramud/client/commands/chat/Say.java
@@ -7,31 +7,30 @@ import com.github.wolfie.kuramud.server.PlayerCharacter;
 
 public class Say implements Command {
 
-  private static final long serialVersionUID = -59431370586086904L;
+    private static final long serialVersionUID = -59431370586086904L;
 
-  private final PlayerCharacter player;
+    private final PlayerCharacter player;
 
-  public Say(final PlayerCharacter player) {
-    this.player = player;
-  }
-
-  @Override
-  public Object execute(final Console console, final String[] argv)
-      throws Exception {
-    String message = "";
-    if (argv != null) {
-      for (final String word : argv) {
-        message += word + " ";
-      }
+    public Say(final PlayerCharacter player) {
+        this.player = player;
     }
-    player.getCurrentRoom().say(
-                player, message);
-    return null;
-  }
 
-  @Override
-  public String getUsage(final Console console, final String[] argv) {
-    return "Use 'say' to speak in the chat";
-  }
+    @Override
+    public Object execute(final Console console, final String[] argv)
+            throws Exception {
+        String message = "";
+        if (argv != null) {
+            for (int i = 1; i < argv.length; i++) {
+                message += argv[i] + " ";
+            }
+        }
+        player.getCurrentRoom().say(player, message);
+        return null;
+    }
+
+    @Override
+    public String getUsage(final Console console, final String[] argv) {
+        return "Use 'say' to speak in the chat";
+    }
 
 }
